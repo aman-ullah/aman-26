25de1c001b4ee97764b267462deb087fbb3c0390
aman-ullah <aman.ullah0123@gmail.com>
2016-01-11 00:27:54 +0600

    8thcommit

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -25,6 +25,55 @@ class Welcome extends REST_Controller {
 		$this->load->helper('url');
 		echo 'high';
 		
+		$oauth_hash = '';
+	$oauth_hash .= 'oauth_consumer_key=hdX5063ycNalkRvVbNDDZdlSN&';
+$oauth_hash .= 'oauth_nonce=' . time() . '&';
+$oauth_hash .= 'oauth_signature_method=HMAC-SHA1&';
+$oauth_hash .= 'oauth_timestamp=' . time() . '&';
+$oauth_hash .= 'oauth_token=429053721-zPe5PTpLld6tLN6MSw2uNhG9fxtC38XBh1xFilzA&';
+$oauth_hash .= 'oauth_version=1.0&';
+$oauth_hash .= 'screen_name=premierleague';
+
+$base = '';
+$base .= 'GET';
+$base .= '&';
+$base .= rawurlencode('https://api.twitter.com/1.1/statuses/user_timeline.json');
+$base .= '&';
+$base .= rawurlencode($oauth_hash);
+$key = '';
+$key .= rawurlencode('TDMnKw2mnpFgRuMHW6tOaceXS0XbT0ciJ0VNK7dmHTaf3NFl56');
+$key .= '&';
+$key .= rawurlencode('xN5ZOEjLD6oyYpI8ezJxqdsOfTy0IF5DY7F5Tq2EZfOmC');
+$signature = base64_encode(hash_hmac('sha1', $base, $key, true));
+$signature = rawurlencode($signature);
+
+
+
+$oauth_header = '';
+$oauth_header .= 'oauth_consumer_key="hdX5063ycNalkRvVbNDDZdlSN", ';
+$oauth_header .= 'oauth_nonce="' . time() . '", ';
+$oauth_header .= 'oauth_signature="' . $signature . '", ';
+$oauth_header .= 'oauth_signature_method="HMAC-SHA1", ';
+$oauth_header .= 'oauth_timestamp="' . time() . '", ';
+$oauth_header .= 'oauth_token="429053721-zPe5PTpLld6tLN6MSw2uNhG9fxtC38XBh1xFilzA", ';
+$oauth_header .= 'oauth_version="1.0", ';
+$oauth_header .= 'screen_name="premierleague"';
+
+$curl_header = array("Authorization: Oauth {$oauth_header}", 'Expect:');
+
+
+$curl_request = curl_init();
+curl_setopt($curl_request, CURLOPT_HTTPHEADER, $curl_header);
+curl_setopt($curl_request, CURLOPT_HEADER, false);
+curl_setopt($curl_request, CURLOPT_URL, 'https://api.twitter.com/1.1/statuses/user_timeline.json');
+curl_setopt($curl_request, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl_request, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl_request, CURLOPT_URL, 'https://api.twitter.com/1.1/statuses/user_timeline.json?count=TOTAL_COUNT_YOU_WANT&screen_name=premierleague');
+
+$json = curl_exec($curl_request);
+curl_close($curl_request);
+
+		
 		$this->load->library('rest', array('server' => 'http://twitter.com/'));
  
 		$user = $this->rest->get('users/show', array('screen_name' => 'Aman Ullah Aman'));
