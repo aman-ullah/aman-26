d6dcac916a83d8d048cba48fb42b1e5f7316630f
aman-ullah <aman.ullah0123@gmail.com>
2016-01-11 00:36:12 +0600

    8thcommit

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,9 +1,9 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-require(APPPATH'.libraries/REST_Controller.php');
+//require(APPPATH'.libraries/REST_Controller.php');
 
 
-class Welcome extends REST_Controller {
+class Welcome extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
